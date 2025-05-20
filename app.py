4c4d7f35d3cf5cc96ace39636c41d5103b8186f6
Muhammad Yasir Khan <101388448+yasir13001@users.noreply.github.com>
2025-05-20 14:13:13 +0500

    dependency update

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_limiter import Limiter
 from flask_cors import CORS
-
+from flask_limiter.util import get_remote_address
 import requests
 import os
+
+
 
 app = Flask(__name__)
 CORS(app, origins=["https://www.themoonai.org"]) 
